b8806f3959a2e515e93ca60ef842d7d1b8fefa55
edilsonjr7 <edilsondesouzalimajunior10.com>
2026-06-05 10:47:27 -0300

    subindo o projeto no git

---
+++ src/main/java/coretech/sistemaCoreTech/SistemaCoreTechApplication.java
@@ -0,0 +1,13 @@
+package coretech.sistemaCoreTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCoreTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCoreTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coretech/sistemaCoreTech/SistemaCoreTechApplicationTests.java
@@ -0,0 +1,13 @@
+package coretech.sistemaCoreTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCoreTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
